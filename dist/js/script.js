0a3e172127de39ed5cf922a396dd43c9150023f4
rfanan <rofianan05@gmail.com>
2022-10-11 22:00:16 +0700

    enable function intro

--- dist/js/script.js
+++ dist/js/script.js
@@ -28,48 +28,47 @@ window.addEventListener('click', function (e) {
     }
 });
 
-console.log('yaaa')
+
 // intro
-// window.addEventListener('DOMContentLoaded', () => {
-//     setTimeout(() => {
-//         letterSpan.forEach((span, idx) => {
-//             setTimeout(() => {
-//                 span.classList.add('active');
-//             }, (idx + 1) * 50)
-//         });
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        letterSpan.forEach((span, idx) => {
+            setTimeout(() => {
+                span.classList.add('active');
+            }, (idx + 1) * 50)
+        });
 
-//         setTimeout(() => {
-//             letterSpan.forEach((span, idx) => {
-//                 setTimeout(() => {
-//                     span.classList.remove('active');
-//                     span.classList.add('fade');
-//                 }, (idx + 1) * 500)
-//             })
-//         }, 2000)
+        setTimeout(() => {
+            letterSpan.forEach((span, idx) => {
+                setTimeout(() => {
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1) * 500)
+            })
+        }, 2000)
 
-//         setTimeout(() => {
-//             intro.style.top = '-100vh';
-//         }, 3000)
-//     })
-// })
+        setTimeout(() => {
+            intro.style.top = '-100vh';
+        }, 3000)
+    })
+})
 
+let say = ["Halo", "Hey", "ola`", "Konnichiwa", "Bonjour", "anyoung", "Welcome"],
+    tl = gsap.timeline(),
+    text = document.querySelector(".text"),
+    delay = 0.30;
 
-// let say = ["Halo", "Hey", "ola`", "Konnichiwa", "Bonjour", "anyoung", "Welcome"]
-// tl = gsap.timeline(),
-//     text = document.querySelector(".text"),
-//     delay = 0.30;
-
-// say.forEach((blurb, i) => {
-//     tl.add(() => text.innerText = blurb, i * delay);
-// });
+say.forEach((blurb, i) => {
+    tl.add(() => text.innerText = blurb, i * delay);
+});
 
 // end intro
+
 
 
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
-    multiplier: 1,
     smartphone: {
         smooth: true
     },
@@ -82,7 +81,7 @@ const navBar = document.querySelector("header");
 
 scroll.on("scroll", (position) => {
 
-    if (position.scroll.y > 80) {
+    if (position.scroll.y > 20) {
         navBar.classList.add("navbar-fixed");
     } else {
         navBar.classList.remove("navbar-fixed");
